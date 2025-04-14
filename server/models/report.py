9d8322343500a71b5467d85272104a6e75ecff38
IsaiahSama <zeldaplayergl@gmail.com>
2025-04-14 18:47:05 -0400

    Added created_at and updated_at fields for Reports

--- server/models/report.py
+++ server/models/report.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime as dt
 from typing import Annotated
 from pydantic import BaseModel, Field
 
@@ -14,6 +15,20 @@ class Report(BaseModel):
         Status, Field(default=Status.REPORTED, description="Report status")
     ]
     severity: Annotated[Severity, Field(description="Report severity")]
+    created_at: Annotated[
+        str,
+        Field(
+            default_factory=lambda: dt.now().strftime("%Y-%m-%d"),
+            description="Date report was created",
+        ),
+    ]
+    updated_at: Annotated[
+        str,
+        Field(
+            default_factory=lambda: dt.now().strftime("%Y-%m-%d"),
+            description="Date report was last updated",
+        ),
+    ]
     user_email: Annotated[
         str, Field(default="", description="The email of the uploader")
     ]
