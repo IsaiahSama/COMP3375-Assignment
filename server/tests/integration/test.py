42fc37ad9cd8422c2922d00e1100c5e7af8c7e76
Jonathan <jonathanrossking246@gmail.com>
2025-04-16 23:08:50 -0400

    Integration test done

--- server/tests/integration/test.py
+++ server/tests/integration/test.py
@@ -15,42 +15,46 @@ BASE_URL = "http://127.0.0.1:8000"
 REGISTRER_PAGE_URL = BASE_URL + "/register"
 LOGIN_PAGE_URL = BASE_URL + "/login"
 PROFILE_PAGE_URL = BASE_URL + "/profile"
-driver = webdriver.Edge()
+
+options = webdriver.EdgeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
 mongo_client = MongoClient(config.mongo_uri)
 mongo_db = mongo_client[config.mongo_db_name]
 user_collection  = mongo_db[Collections.USER.value]
 report_collection = mongo_db[Collections.REPORT.value]
 
-# #Registration Integration Test
-# driver.get(REGISTRER_PAGE_URL) #load registration page
-# first_name_input = driver.find_element(By.ID, "firstname") #gert
-# last_name_input = driver.find_element(By.ID, "lastname")
-# email_input = driver.find_element(By.ID, "email")
-# password_input = driver.find_element(By.ID, "password")
+# Registration Integration Test
+driver = webdriver.Edge(options=options)
+driver.get(REGISTRER_PAGE_URL) #load registration page
+first_name_input = driver.find_element(By.ID, "firstname") #gert
+last_name_input = driver.find_element(By.ID, "lastname")
+email_input = driver.find_element(By.ID, "email")
+password_input = driver.find_element(By.ID, "password")
 
-# first_name_input.send_keys("John")
-# last_name_input.send_keys("Doe")
-# email_input.send_keys("JohnDoe@mail.com")
-# password_input.send_keys("bcJ0d^`76M<2")
+first_name_input.send_keys("John")
+last_name_input.send_keys("Doe")
+email_input.send_keys("JohnDoe@mail.com")
+password_input.send_keys("bcJ0d^`76M<2")
 
-# driver.find_element(By.ID, "submit").click()
+driver.find_element(By.ID, "submit").click()
 
-# sleep(5)
+sleep(2)
 
-# is_user_created = user_collection.find_one({"email": "JohnDoe@mail.com"})
+is_user_created = user_collection.find_one({"email": "JohnDoe@mail.com"})
 
-# assert is_user_created is not None
-# assert is_user_created["email"] == "JohnDoe@mail.com"
-# assert is_user_created["first_name"] == "John"
-# assert is_user_created["last_name"] == "Doe"
-# assert is_user_created["role"] == "user"
+assert is_user_created is not None
+assert is_user_created["email"] == "JohnDoe@mail.com"
+assert is_user_created["first_name"] == "John"
+assert is_user_created["last_name"] == "Doe"
+assert is_user_created["role"] == "user"
 
-# print("User Creatinon Test Passed!")
+print("User Creatinon Test Passed!")
 
-# driver.close()
+driver.close()
 
 
 #Login integration test
+driver = webdriver.Edge(options=options)
 driver.get(LOGIN_PAGE_URL)
 email_input = driver.find_element(By.ID, "email")
 password_input = driver.find_element(By.ID, "password")
@@ -59,10 +63,7 @@ sleep(2)
 
 email_input.send_keys("JohnDoe@mail.com")
 password_input.send_keys("bcJ0d^`76M<2")
-
 driver.find_element(By.ID, "submit").click()
-
-
 
 sleep(2)
 
@@ -71,7 +72,6 @@ assert driver.find_element(By.ID, "reportButton").text == "New Report"
 sleep(2)
 
 driver.get(PROFILE_PAGE_URL)
-# driver.find_element(By.ID, "profile").click()
 
 sleep(2)
 
@@ -83,22 +83,5 @@ print("Login Test Passed!")
 driver.close()
 
 
-# driver.get(LOGIN_PAGE_URL)
-
-# email_input = driver.find_element(By.ID, "email")
-
-# email_input.send_keys("sample@mail.com")
-
-# password_input = driver.find_element(By.ID, "password")
-# password_input.send_keys("Short")
-
-# driver.find_element(By.ID, "submit").click()
-
-# print("Checking Credentials")
-# assert driver.find_element(By.ID, "errorMessage").text == "Invalid Credentials"
-
-# print("Success!")
-
-# _ = input()
 
 
